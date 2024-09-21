ff7c357e0f35d8e2139452626da93464623b81e3
D-future0 <oka4collins@gmail.com>
2024-09-21 09:21:58 +0100

    total and amount setup

--- 14-shopping-cart/src/context.js
+++ 14-shopping-cart/src/context.js
@@ -1,40 +1,57 @@
-import React, { useContext, useReducer } from 'react'
-import { createContext } from 'react'
-import CartItems from './data'
-import reducer from './reducer'
+import React, { useContext, useReducer, useEffect } from "react";
+import { createContext } from "react";
+import CartItems from "./data";
+import reducer from "./reducer";
+const url = `https://www.course-api.com/react-useReducer-cart-project`
 
-const AppContext = createContext()
-const AppProvider = ({children}) => {
-  const defaultState ={
+const AppContext = createContext();
+const AppProvider = ({ children }) => {
+  const defaultState = {
     loading: false,
     cart: CartItems,
-    total:0,
-    amount: 0
+    total: 0,
+    amount: 0,
+  };
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+  const remove = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+  const fetchData = async()=>{
+    dispatch({type:'LOADING'})
+    const resp = await fetch(url)
+    const cart = await resp.json()
+    dispatch({type:'DISPLAY_CART', payload:cart})
   }
-    const [state, dispatch] = useReducer(reducer, defaultState)
-    const clearCart =()=>{
-      dispatch({ type:'CLEAR_CART' })
-    }
-    const remove =(id)=>{
-      dispatch({ type:'REMOVE', payload: id})
-    }
-    const increase =(id)=>{
-      dispatch({ type:'INCREASE', payload: id})
-    }
-    const decrease =(id)=>{
-      dispatch({ type:'DECREASE', payload: id})
-    }
+  useEffect(()=>{
+    fetchData()
+  }, [])
+  useEffect(() => {
+    dispatch({ type: "GET_TOTAL" });
+  }, [state.cart]);
   return (
-    <AppContext.Provider value={{
+    <AppContext.Provider
+      value={{
         ...state,
         clearCart,
         remove,
         increase,
-        decrease, 
-    }}>{children}</AppContext.Provider>
-  )
-}
-const useGlobalContext = ()=>{
-    return useContext(AppContext)
-}
-export { AppProvider, useGlobalContext }
+        decrease,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+export { AppProvider, useGlobalContext };
--- 14-shopping-cart/src/reducer.js
+++ 14-shopping-cart/src/reducer.js
@@ -21,9 +21,27 @@ const reducer = (state, action) => {
             return{...item, amount: item.amount - 1}
         }
         return item
-    })
+    }).filter(item => item.amount !== 0)
     return {...state, cart: amountTemp}
   }
+  if(action.type === 'GET_TOTAL'){
+    let {total, amount} = state.cart.reduce((cartTotal, items)=>{
+      const {price, amount} = items
+      const tempTotal = price * amount
+      cartTotal.total += tempTotal
+      cartTotal.amount += amount
+      return cartTotal
+    },{total:0, amount:0})
+    total = parseFloat((total).toFixed(2))
+    return {...state, total, amount}
+  }
+  if(action.type === 'LOADING'){
+    return {...state, loading:true}
+  }
+  if(action.type === 'DISPLAY_CART'){
+    return{...state, cart: action.payload, loading: false}
+  }
+  throw new Error('action type does not match')
 }
 
 export default reducer
